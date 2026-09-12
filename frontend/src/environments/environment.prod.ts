37dfbc3bd177a4a8a23d3c51d454ca8c036e2066
MKbenjA9 <xbenjamin629@gmail.com>
2026-09-12 11:18:11 -0300

    Fix production Azure redirect URI

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,16 +1,20 @@
-/**
- * Variables de entorno para Produccion
- */
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+
+  apiUrl: 'https://jfbxfcaajg.execute-api.us-east-1.amazonaws.com',
+
   azure: {
     clientId: 'c1ceeea6-1f0a-46ba-b79d-ed659608feef',
     tenantId: '16c2a7cf-f000-475f-a847-f95ee0b36404',
-    redirectUri: 'https://tudominio.com',
-    authority: 'https://login.microsoftonline.com/16c2a7cf-f000-475f-a847-f95ee0b36404',
+
+    redirectUri: 'https://alvasmash.github.io/CloudeNative/redirect',
+
+    authority: 'https://login.microsoftonline.com/16c2a7cf-f000-475f-a847-f95ee0b36404/v2.0',
+
     apiScope: 'api://aef48164-1df8-49e6-b9f2-c1d8953ea200/recurso.write',
+
     loginScopes: ['openid', 'profile', 'email']
   },
+
   demoMode: false
 };
